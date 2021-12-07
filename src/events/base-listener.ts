fa7dde8652fc9ef524a47baefd148e8131d961e2
Omid Rouhani <o.rohani@gmail.com>
2021-12-07 04:57:25 +0330

    base-listener modified

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -1,12 +1,15 @@
 import { consumerOpts, createInbox, JsMsg, JSONCodec, NatsConnection } from 'nats';
+import { Streams } from './streams';
 import { Subjects } from "./subjects";
 
 interface Event {
+  stream: Streams;
   subject: Subjects;
   data: any;
 }
 
 export abstract class Listener<T extends Event> {
+  abstract stream: T[ 'stream' ];
   abstract subject: T[ 'subject' ];
   abstract queueGroupName: string;
   abstract onMessage ( data: T[ 'data' ], msg: JsMsg ): void;
@@ -30,6 +33,16 @@ export abstract class Listener<T extends Event> {
   }
 
   async listen () {
+    const jsm = await this.natsConnection.jetstreamManager();
+
+    try {
+      // check if stream is existed
+      await jsm.streams.info( this.stream );
+    } catch ( err ) {
+      // stream not found so we add it
+      await jsm.streams.add( { name: this.stream, subjects: [ `${ this.stream }.*` ] } );
+    }
+
     const jetStreamClient = this.natsConnection.jetstream();
     const subscription = await jetStreamClient.subscribe(
       this.subject,
--- src/events/taxonomy-created-listener.ts
+++ src/events/taxonomy-created-listener.ts
@@ -1,9 +1,11 @@
 import { JsMsg } from "nats";
 import { Listener } from "./base-listener";
+import { Streams } from "./streams";
 import { Subjects } from "./subjects";
 import { TaxonomyCreatedEvent } from "./taxonomy-created-event";
 
 export class TaxonomyCreatedListener extends Listener<TaxonomyCreatedEvent> {
+  stream: Streams.Taxonomy = Streams.Taxonomy;
   subject: Subjects.TaxonomyCreated = Subjects.TaxonomyCreated;
   queueGroupName = 'taxonomy-service';
   onMessage ( data: TaxonomyCreatedEvent[ 'data' ], msg: JsMsg ) {
